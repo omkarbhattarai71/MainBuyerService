9b1dae9dc6289704393ab2add2627f840144c6fd
Omkar Bhattarai <omkarbhattarai71@gmail.com>
2023-06-27 03:44:12 +0530

    First Comit
    Beginning of Microservices

---
+++ src/test/java/com/example/mainbuyerservice/MainBuyerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainbuyerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBuyerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
